d613a60909f65b542bec50bd9b4069b8ef690463
Hany Bakir <hb185062@ncratleos.com>
2024-11-16 12:23:15 +0300

    On branch main  Initial commit  Changes to be committed: 	new file:   Makefile 	new file:   SimpleList 	new file:   main.d 	new file:   obj/main.o 	new file:   src/main.cpp

---
+++ src/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+void print_menu(string name);
+
+int main(int arg_count, char *args[])
+{
+    //cout << arg_count << endl;
+
+    if (arg_count > 1)
+    {
+        string name (args[1]);
+        print_menu(name);
+    }
+    else
+    {
+        cout << "Username not supplied.. exiting the program" << endl;
+    }
+    
+    return 0;
+}
+
+void print_menu(string name)
+{
+    int choice;
+
+    cout << "*********************\n";
+    cout << " 1 - Print list.\n";
+    cout << " 2 - Add to list.\n";
+    cout << " 3 - Delete from list.\n";
+    cout << " 4 - Quit.\n";
+    cout << " Enter your choice and press return/enter.\n";
+
+    cin >> choice;
+
+    if(choice == 4)
+    {
+        exit(0);
+    }
+    else
+    {
+        cout << "Sorry choice hasn't been implemented.\n";
+    }
+}
